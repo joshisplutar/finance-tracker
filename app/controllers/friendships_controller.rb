cb98236cbfb6307b731fff968eac623447783438
joshisplutar <joshisplutar@gmail.com>
2020-06-25 08:46:15 -0500

    Add option to follow friends from friends search view

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "You are now stalking somebody new."
+    else
+      flash[:notice] = "Oops, that didn't work."
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
